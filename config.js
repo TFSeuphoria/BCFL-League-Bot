4bc7ada9df285bb332a0684294315cc31bc747a1
TFSeuphoria <jacksonl5941@gmail.com>
2025-10-23 22:38:13 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // Role IDs
+  roles: {
+    commissioner: "ROLE_ID_HERE",
+    streamer: "ROLE_ID_HERE",
+    referee: "ROLE_ID_HERE",
+    franchiseOwner: "ROLE_ID_HERE",
+    generalManager: "ROLE_ID_HERE",
+    headCoach: "ROLE_ID_HERE",
+    assistantCoach: "ROLE_ID_HERE",
+    suspended: "ROLE_ID_HERE",
+  },
+
+  // Channel IDs
+  channels: {
+    pickups: "CHANNEL_ID_HERE",
+    transactions: "CHANNEL_ID_HERE",
+    streams: "CHANNEL_ID_HERE",
+    agency: "CHANNEL_ID_HERE",
+    gametimes: "CHANNEL_ID_HERE",
+  },
+};
